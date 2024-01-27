e844c6d9bd6fc1d44aeb0a61463106a825d3851a
Speyll <speyll@protonmail.com>
2024-01-27 23:32:33 +0100

    pipewire script updated

--- .config/sway/start-pipewire.sh
+++ .config/sway/start-pipewire.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-# Check if Pipewire is running
-if ! pgrep -x "pipewire" > /dev/null; then
-  echo "Pipewire is not running. Starting..."
-  dbus-run-session pipewire &
-  sleep 1
-  wireplumber &
-  pipewire-pulse &
-else
-  echo "Pipewire is already running."
-fi
+# Kill existing Pipewire processes
+pkill -x pipewire
+pkill -x wireplumber
+pkill -x pipewire-pulse
+
+# Start a fresh Pipewire session
+echo "Starting Pipewire..."
+dbus-run-session pipewire &
+sleep 1
+wireplumber &
+pipewire-pulse &
+
+echo "Pipewire is ready to rock!"
