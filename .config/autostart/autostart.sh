26fb040c45e7c42914294823e065f7cfc222b608
Speyll <speyll@protonmail.com>
2024-03-15 22:24:31 +0100

    updated some minor things

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
 
-# Function to kill process by name
-kill_process() {
-    local pid=$(pgrep "$1")
-    if [ -n "$pid" ]; then
-        kill "$pid"
-    fi
-}
+# Start Pipewire
+$HOME/.config/autostart/start-pipewire.sh &
 
 # Set wallpaper with swaybg
 swaybg --mode fill -i "$HOME/pictures/wall.jpg" &
 
-# Start Pipewire
-"$HOME/.config/autostart/start-pipewire.sh" &
-
 # Kill and start Mako
-kill_process mako
+pkill -x mako
 mako &
 
 # Kill and start Night light
-kill_process wlsunset
+pkill -x wlsunset
 wlsunset -l 36.7 -L 3.08 &
--- .config/autostart/start-pipewire.sh
+++ .config/autostart/start-pipewire.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
-# Function to kill process by name
-kill_process() {
-    local pid=$(pgrep "$1")
-    if [ -n "$pid" ]; then
-        kill "$pid"
-    fi
-}
-
 # Kill existing Pipewire processes
-kill_process pipewire
-kill_process wireplumber
-kill_process pipewire-pulse
+pkill -x pipewire
+pkill -x wireplumber
+pkill -x pipewire-pulse
 
 # Start a fresh Pipewire session
-echo "Starting Pipewire..."
 dbus-run-session pipewire &
 sleep 1
 wireplumber &
