7497e53f7f0728150ab003c59209b2c26fb04146
Speyll <speyll@protonmail.com>
2024-03-18 00:18:01 +0100

    fixed cursors

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -6,6 +6,10 @@ $HOME/.config/autostart/start-pipewire.sh &
 # Set wallpaper with swaybg
 swaybg --mode fill -i "$HOME/pictures/wall.jpg" &
 
+# Set cursor
+gsettings set org.gnome.desktop.interface cursor-theme Breeze_Snow &
+seat seat0 xcursor_theme Breeze_Snow &
+
 # Kill and start Mako
 pkill -x mako
 mako &
