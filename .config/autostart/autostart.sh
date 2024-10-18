04a1c9b0e1e7edce54f67f1eaa8cf2b88c7d7e0c
speyll <speyll@protonmail.com>
2024-10-18 05:28:40 +0100

    reformated a bunch of configs, automatic tmux, and more

--- .config/autostart/autostart.sh
+++ .config/autostart/autostart.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Update DBus session
+# Environment Setup
 dbus-update-activation-environment --all &
 
-# Start polkit agent
+# Polkit Agent
 /usr/libexec/polkit-gnome-authentication-agent-1 &
 
-# Set output and adaptive sync
+# Output and Display Configuration
 wlr-randr --output HDMI-A-1 --mode 1920x1080@60.000000 --adaptive-sync enabled &
 
-# Kill existing processes to avoid duplicates
+# Kill Existing Processes
 pkill -x pipewire
 pkill -x wireplumber
 pkill -x pipewire-pulse
@@ -20,7 +20,7 @@ pkill -x wlsunset
 pkill -x waybar
 pkill -x cliphist
 
-# Start PipeWire and related services
+# Start PipeWire and Services
 dbus-run-session pipewire &
 sleep 1
 wireplumber &
@@ -28,27 +28,24 @@ pipewire-pulse &
 
 # Start Waybar
 waybar -c $HOME/.config/waybar/stacking-config -s $HOME/.config/waybar/style.css &
+# waybar -c $HOME/.config/waybar/tiling-config -s $HOME/.config/waybar/style.css &
 
-# Set wallpaper with swaybg or mpvpaper
-#swaybg --mode fill -i "$HOME/pictures/walls/wall.jpg" &
+# Set Wallpaper
 swaybg -i $(find $HOME/pictures/walls/*.jpg -type f | shuf -n1) -m fill &
-#mpvpaper -vsp -o "no-audio pause=no --loop --ytdl-format='bestvideo[height<=1080]+bestaudio/best'" '*' "$HOME/pictures/walls/wall.mp4" &
-#mpvpaper -vsp -o "no-audio pause=no --loop --ytdl-format='bestvideo[height<=1080]+bestaudio/best'" '*' $(find $HOME/pictures/walls/*.mp4 -type f | shuf -n1) &
+# swaybg --mode fill -i "$HOME/pictures/walls/wall.jpg" &
+# mpvpaper -vsp -o "no-audio pause=no --loop --ytdl-format='bestvideo[height<=1080]+bestaudio/best'" '*' "$HOME/pictures/walls/wall.mp4" &
+# mpvpaper -vsp -o "no-audio pause=no --loop --ytdl-format='bestvideo[height<=1080]+bestaudio/best'" '*' $(find $HOME/pictures/walls/*.mp4 -type f | shuf -n1) &
 
-# Set cursor theme
+# Cursor Theme
 gsettings set org.gnome.desktop.interface cursor-theme Breeze_Snow &
 seat seat0 xcursor_theme Breeze_Snow &
 
-# Start notifications
-fnott &
+# Start Additional Services
+fnott &                  # Notifications
+wlsunset -l 36.7 -L 3.08 &  # Night light
+wl-paste --watch cliphist store -max-items 100 &  # Clipboard manager
 
-# Start Night light
-wlsunset -l 36.7 -L 3.08 &
-
-# Start clipboard manager
-wl-paste --watch cliphist store -max-items 100 &
-
-# Start xdg-desktop-portal services
+# Start XDG Desktop Portal Services
 /usr/libexec/xdg-desktop-portal &
 /usr/libexec/xdg-desktop-portal-gtk &
 /usr/libexec/xdg-desktop-portal-wlr &
