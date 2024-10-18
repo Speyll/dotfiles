533cd1eec2de0d4f4d5d5f502452d567e6e393e3
speyll <speyll@protonmail.com>
2024-10-18 09:08:37 +0100

    simplified the sway waybar keyboard toggle, if ur watching this repo i'm sorry for spamming

--- .config/waybar/scripts/sway-keyboard-toggle.sh
+++ .config/waybar/scripts/sway-keyboard-toggle.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 # Toggle between 'us' and 'fr' keyboard layouts
-if [ -f /tmp/keyboard_layout_us ]; then
-    swaymsg input "*" xkb_layout fr
-    rm /tmp/keyboard_layout_us
-else
-    swaymsg input "*" xkb_layout us
-    touch /tmp/keyboard_layout_us
-fi
+swaymsg input '*' xkb_switch_layout next
