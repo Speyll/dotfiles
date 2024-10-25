dfe9df6ad9703493a8a2a4a83ccd40e778a16f60
speyll <speyll@protonmail.com>
2024-10-25 13:49:01 +0100

    added mini.pairs plugin to nvim

--- .config/nvim/lua/speyll/core/set.lua
+++ .config/nvim/lua/speyll/core/set.lua
@@ -1,4 +1,5 @@
 vim.opt.guicursor = ""
+vim.opt.cursorline = false
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
--- .config/nvim/lua/speyll/plugins/telescope.lua
+++ .config/nvim/lua/speyll/plugins/telescope.lua
@@ -2,11 +2,14 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        local home = vim.fn.expand("~") -- Expands the home directory
+
         require('telescope').setup{
             defaults = {
                 file_ignore_patterns = {
-                    "%.jpg", "%.jpeg", "%.png", "%.gif", "%.bmp", "%.tiff", "%.webp", "%.kra",
-                    "%.mp4", "%.mkv", "%.webm", "%.avi", "%.mov", "%.flv", "%.wmv"
+                    "%.jpg", "%.jpeg", "%.png", "%.gif", "%.bmp", "%.tiff", "%.webp", "%.kra", -- image formats
+                    "%.mp4", "%.mkv", "%.webm", "%.avi", "%.mov", "%.flv", "%.wmv", -- video formats
+                    "%.mp3", "%.wav", "%.flac", "%.m4a", -- audio formats
                 }
             }
         }
